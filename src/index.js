09222abd8b99652f8790cbab910dd1deffec465a
sja_thedude <sja.affu765@gmail.com>
2021-11-16 20:48:45 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,127 +1,143 @@
-import { isComp, checkBox } from './script';
-import {
-  listUpdate, addToDo, clearAll, clearAllComp, itemDelete, capitalize,
-} from './addremove';
 import './style.css';
 
-let dataList = [];
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const appId = 'AZT0GFy9XFpC4qapJXTL';
 
-const index = (dataList) => {
-  for (let i = 0; i < dataList.length; i++) { /* eslint-disable-line no-plusplus */
-    dataList[i].index = i + i;
+const commentCounter = () => {
+  const commentsArr = document.querySelectorAll('.comment-item');
+  const commentsHeader = document.querySelector('#comments-head');
+  if (commentsArr) {
+    commentsHeader.textContent = `Comments (${commentsArr.length}) by previous visitors`;
+  } else {
+    commentsHeader.textContent = 'Comments (0) by previous visitors';
   }
-
-  return dataList;
 };
 
-const saveToLocalStorage = (dataList) => {
-  localStorage.setItem('todo_list', JSON.stringify(dataList));
-};
-
-const refreshPage = () => {
-  window.location.reload();
-};
-
-const component = () => {
-  const todoContainer = document.querySelector('.todo-list-con');
-  let element = document.createElement('li');
-  element.className = 'todo-item';
-
-  const heading = document.createElement('h2');
-  heading.className = 'heading';
-  heading.textContent = 'Today\'s To Do';
-  element.appendChild(heading);
-
-  const clear = document.createElement('button');
-  clear.className = 'clear';
-  clear.innerHTML = '<i class=\'sync alternate icon\'></i>';
-  element.appendChild(clear);
-  todoContainer.appendChild(element);
-
-  clearAll(clear, dataList, saveToLocalStorage, refreshPage);
-
-  element = document.createElement('li');
-  element.className = 'todo-item';
-
-  const addItem = document.createElement('input');
-  addItem.className = 'add-item';
-  addItem.placeholder = 'Add to your list';
-  addItem.value = '';
-  element.appendChild(addItem);
-
-  const btn = document.createElement('button');
-  btn.className = 'enter-button';
-  btn.innerHTML = '<i class=\'level down alternate icon\'></i>';
-  element.appendChild(btn);
-  todoContainer.appendChild(element);
-
-  addItem.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      addToDo(addItem.value, dataList);
-      saveToLocalStorage(dataList);
-      refreshPage();
-    }
-  });
-
-  btn.addEventListener('click', () => {
-    addToDo(addItem.value, dataList);
-    saveToLocalStorage(dataList);
-    refreshPage();
-  });
-
-  if (dataList.length !== 0) {
-    dataList.forEach((todo) => {
-      element = document.createElement('li');
-      element.className = 'todo-item';
-
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.className = 'checkbox';
-      checkbox.checked = todo.completed;
-      element.appendChild(checkbox);
-
-      const description = document.createElement('textarea');
-      description.className = 'description';
-      description.rows = 'auto';
-      description.value = capitalize(todo.description);
-      element.appendChild(description);
-
-      const taskButton = document.createElement('button');
-      taskButton.className = 'task-button';
-      taskButton.innerHTML = '<i class=\'ellipsis vertical icon\'></i>';
-      element.appendChild(taskButton);
-
-      checkBox(checkbox, todo, dataList, saveToLocalStorage, refreshPage);
-      isComp(todo.completed, description);
-
-      todoContainer.appendChild(element);
+const commentsLoad = (itemId) => {
+  fetch(`${url}${appId}/comments?item_id=${itemId}`)
+    .then((response) => response.json())
+    .then((json) => {
+      if (json) {
+        const itemArr = json;
+        const commentsDiv = document.querySelector('#comments');
+        let commentsHtml = '';
+        itemArr.forEach((item) => {
+          commentsHtml += `<p class="comment-item">${item.creation_date} by ${item.username} : ${item.comment}`;
+        });
+        commentsDiv.innerHTML = commentsHtml;
+        commentCounter();
+        return 0;
+      }
+      return 0;
     });
-  }
-
-  listUpdate(dataList, saveToLocalStorage, refreshPage);
-
-  itemDelete(dataList, index, saveToLocalStorage, refreshPage);
-
-  element = document.createElement('li');
-
-  const clearCompleted = document.createElement('button');
-  clearCompleted.className = 'clear-completed';
-  clearCompleted.innerHTML = 'Clear all completed';
-  element.appendChild(clearCompleted);
-  todoContainer.appendChild(element);
-
-  clearAllComp(clearCompleted, dataList, index, saveToLocalStorage, refreshPage);
 };
 
-const pageLoad = () => {
-  window.onload = () => {
-    if (localStorage.getItem('todo_list') !== null) {
-      dataList = JSON.parse(localStorage.getItem('todo_list'));
-      component();
-    } else {
-      component();
-    }
-  };
+const commentUpload = (obj) => {
+  fetch(`${url}${appId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then(() => {
+
+    });
 };
 
-pageLoad();
+const loadPopupCommentPage = (itemId, popupNode) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemId}`)
+    .then((response) => response.json())
+    .then((json) => {
+      const meal = json.meals[0];
+      const popupHtml = `
+            <div class="con">
+            <i class="fas fa-times fa-2x" id="back-menu"></i>
+                <div id="img-comment" class="img-comment">
+                <img src="${meal.strMealThumb}" alt="meal-img">
+                <h5>${meal.strMeal}</h5>
+                </div>
+                <div id="info-item-comment" class="item-info">
+                <div class="ingredient">
+                <h5>Ingredient <i class="fas fa-arrow-down"></i></h5>
+                <ul>
+                <li>${meal.strIngredient1}</li>
+                <li>${meal.strIngredient2}</li>
+                <li>${meal.strIngredient3}</li>
+                <li>${meal.strIngredient4}</li>
+                <li>${meal.strIngredient5}</li>
+                </ul>
+                </div>
+                <div class="instructions"> 
+                <h5>Instruction <i class="fas fa-note"></i></h5>
+                    <p> ${meal.strInstructions}</p>
+                    </div>
+                <a href=${meal.strSource} target="_blank">See more about this meal <i class="fas fa-arrow-right"></i></a>
+                </div>
+                <h4 id="comments-head">Comments By previous Visitors</h4>
+                <div id="comments" class="comments"></div>
+                <div id="form-comment" class="form-comment">
+                <form id="comment-form">
+                <div class="form-group">
+                <input type="text" class="form-control" id="your-name" name="name" placeholder="Your name" required>
+                </div>
+                <div class="form-group">
+                <textarea class="form-control" id="your-comments" name="comment" placeholder="Your comment" required></textarea>
+                </div>
+                <button type="submit" class="btn-submit">Comment</button>
+                </form>
+                </div>
+                </div>
+                `;
+      popupNode.innerHTML = popupHtml;
+      commentsLoad(meal.idMeal);
+
+      const commentForm = document.querySelector('#comment-form');
+      commentForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const commentObj = {
+          item_id: meal.idMeal,
+          username: commentForm.name.value,
+          comment: commentForm.comment.value,
+        };
+        commentUpload(commentObj);
+        commentForm.name.value = '';
+        commentForm.comment.value = '';
+        setTimeout(() => { commentsLoad(meal.idMeal); }, 500);
+      });
+
+      const header = document.querySelector('header');
+      const main = document.querySelector('.row');
+      const footer = document.querySelector('footer');
+      const popupComment = document.querySelector('#popup-comment');
+
+      const goBack = document.querySelector('#back-menu');
+      goBack.addEventListener('click', () => {
+        popupNode.innerHTML = '';
+        header.style.filter = 'none';
+        main.style.filter = 'none';
+        footer.style.filter = 'none';
+        popupComment.style.display = 'none';
+      });
+    });
+};
+
+const header = document.querySelector('header');
+const main = document.querySelector('.row');
+const footer = document.querySelector('footer');
+const popupComment = document.querySelector('#popup-comment');
+const commentBtn = document.querySelectorAll('#btn-comment');
+
+commentBtn.forEach((item) => {
+  item.addEventListener('click', () => {
+    header.style.filter = 'blur(4px)';
+    main.style.filter = 'blur(4px)';
+    footer.style.filter = 'blur(4px)';
+    popupComment.style.display = 'block';
+
+    const itemId = item.parentNode.parentNode.querySelector('img').id;
+
+    loadPopupCommentPage(itemId, popupComment);
+  });
+});
